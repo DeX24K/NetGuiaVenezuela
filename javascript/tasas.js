0dc886f21587daf3d7d416c5b667884eefe88b61
DeX24K <kleylervegas0@gmail.com>
2026-07-12 19:51:16 -0400

    cambiosx

--- javascript/tasas.js
+++ javascript/tasas.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
             const tickerContent = document.getElementById('ticker-content');
             tickerContent.innerHTML = `
                 <span class="ticker-item">💵 USD: <strong>${data.dolar} Bs</strong></span>
-                <span class="ticker-item">🇪🇺 EUR: <strong>${data.euro} Bs</strong></span>
+                <span class="ticker-item">💶 EUR: <strong>${data.euro} Bs</strong></span>
                 <span class="ticker-item">💎 USDT: <strong>${data.usdt} Bs (precio BINANCE)</strong></span>
                 <span class="ticker-fecha">| Fecha: ${data.fecha}</span>
             `;
